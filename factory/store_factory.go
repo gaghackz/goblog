cd77ee6cca2fe5ce3b24ef2af5e109df5abdb65a
gaghackz <karanamgagan@gmail.com>
2025-06-22 23:32:17 +0530

    second commit

--- factory/store_factory.go
+++ factory/store_factory.go
@@ -10,7 +10,7 @@ import (
 
 func NewBlogStore(app *gofr.App) store.BlogStore {
 	
-	db, err := sql.Open("mysql", "root:password@localhost:2001/test")
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:2001)/test")
 	if err != nil {
 		panic("failed to connect to database")
 	}
@@ -20,5 +20,18 @@ func NewBlogStore(app *gofr.App) store.BlogStore {
 		panic("cannot connect to dockerized MySQL")
 	}
 
+	schema := `
+	CREATE TABLE IF NOT EXISTS blogs (
+		id VARCHAR(50) PRIMARY KEY,
+		title TEXT NOT NULL,
+		content TEXT NOT NULL,
+		author TEXT NOT NULL
+	);`
+
+	if _, err := db.Exec(schema); err != nil {
+		panic("failed to create blogs table: " + err.Error())
+	}
+
 	return store.New(db)
+
 }
